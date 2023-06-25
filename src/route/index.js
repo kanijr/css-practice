e06b1f7e9bdd28a1ad7cab8dcb71b7de6fad4c9a
kanijr <zitfitgpo@gmail.com>
2023-06-25 12:55:59 +0300

    created dashboard page

--- src/route/index.js
+++ src/route/index.js
@@ -25,6 +25,15 @@ router.get('/slack', function (req, res) {
 })
 // ================================================================
 
+router.get('/dashboard', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('dashboard', { layout: null })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+// ================================================================
+
 router.get('/selector', function (req, res) {
   // res.render генерує нам HTML сторінку
 
